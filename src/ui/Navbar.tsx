a7ed1138f6071d223f6a1ce1eb1d42ba708f4aec
Sandro <vipsandro11@gmail.com>
2023-10-16 23:43:34 +0400

    style:change navbar style and responsivness

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -1,9 +1,9 @@
 import logo from "/logo/logo-no-background.png";
 const Navbar = () => {
   return (
-    <header>
+    <header className="bg-white shadow-lg">
       <nav className="flex-between section-center p-4">
-        <img src={logo} alt="logo" className="w-full max-w-[3.5rem]" />
+        <img src={logo} alt="logo" className="w-full max-w-[3.5rem] cursor-pointer" />
         <ul className="flex-center gap-5">
           <li className="hover:text-teal cursor-pointer transition-colors ease-in duration-150">Mail</li>
           <li className="hover:text-teal cursor-pointer transition-colors ease-in duration-150">IMEI</li>
