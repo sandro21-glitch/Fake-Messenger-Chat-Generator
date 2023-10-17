5424a977f831f5bbe2d2c8fd6d3b6db976fa55dd
Sandro <vipsandro11@gmail.com>
2023-10-17 18:45:07 +0400

    feat: Revamp Home component structure

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,14 @@
-
+import LeftPanel from "../features/LeftPanel/LeftPanel";
+import RightPanel from "../features/RightPanel/RightPanel";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="section-center p-4">
+      <article className="flex gap-5">
+        <LeftPanel />
+        <RightPanel />
+      </article>
+    </section>
+  );
 };
 export default Home;
