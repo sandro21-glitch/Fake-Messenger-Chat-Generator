a71f964dcc4db6729f90ebd81ba93b9857c9c17b
Sandro <vipsandro11@gmail.com>
2023-10-18 15:51:18 +0400

    refactor: Update Layout component to use state and context for layout selection

--- src/features/LeftPanel/Layout.tsx
+++ src/features/LeftPanel/Layout.tsx
@@ -1,19 +1,45 @@
+import { useState, useEffect } from "react";
 import TextHeading from "../../ui/TextHeading";
+import { useChatContext } from "../../context/ChatContext";
 
 const Layout = () => {
+  const [activeLayout, setActiveLayout] = useState<string>("android");
+  const { handleActiveLayout } = useChatContext();
+  const handleLayoutChange = (layout: string) => {
+    setActiveLayout(layout);
+  };
+
+  useEffect(() => {
+    handleActiveLayout(activeLayout);
+  }, [activeLayout]);
+
   return (
     <div className="w-1/2">
       <TextHeading text="Layout :" />
       <div>
-        <div>
+        <div className="flex items-center">
           <label htmlFor="android" className="text-labels cursor-pointer">
-            <input type="radio" name="android" id="android" className="mr-1" />
+            <input
+              type="radio"
+              name="layout"
+              id="android"
+              className="mr-1"
+              checked={activeLayout === "android"}
+              onChange={() => handleLayoutChange("android")}
+            />
             android
           </label>
         </div>
-        <div>
+        <div className="flex items-center">
           <label htmlFor="iphone" className="text-labels cursor-pointer">
-            <input type="radio" name="iphone" id="iphone" className="mr-1" />
+            <input
+              type="radio"
+              name="layout"
+              id="iphone"
+              className="mr-1"
+              checked={activeLayout === "iphone"}
+              onChange={() => handleLayoutChange("iphone")}
+            />
             iphone
           </label>
         </div>
@@ -21,4 +47,5 @@ const Layout = () => {
     </div>
   );
 };
+
 export default Layout;
