d3649948a7a424f05d2886f85ddaf746fd48916a
Sandro <vipsandro11@gmail.com>
2023-10-18 15:56:41 +0400

    feat: Define StateTypes to structure chat application state

---
+++ src/types/StateTypes.ts
@@ -0,0 +1,15 @@
+type StateTypes = {
+  personOne: string[];
+  personTwo: string[];
+  parts: {
+    activeNow: boolean;
+    hideHeader: boolean;
+    hideFooter: boolean;
+  };
+  layout: string;
+  battery: string;
+  img: string;
+  name: string;
+};
+
+export default StateTypes;
