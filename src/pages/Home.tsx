6920c677f3ec5dc3d3538f454af068b8a143e297
Sandro <vipsandro11@gmail.com>
2023-10-18 15:56:16 +0400

    feat: Update Home component to handle and pass view-related props

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import LeftPanel from "../features/LeftPanel/LeftPanel";
 import RightPanel from "../features/RightPanel/RightPanel";
 
 const Home = () => {
+  const [activeView, setActiveView] = useState<string>("mobile");
+
   return (
-    <section className="section-center p-4">
+    <section className="section-center px-4 py-20">
       <article className="flex gap-5">
-        <LeftPanel />
-        <RightPanel />
+        <LeftPanel activeView={activeView} />
+        <RightPanel activeView={activeView} setActiveView={setActiveView} />
       </article>
     </section>
   );
