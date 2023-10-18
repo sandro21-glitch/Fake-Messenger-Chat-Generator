3821f429a7fb4a6383b06abfd1ebb6df160fb8f3
Sandro <vipsandro11@gmail.com>
2023-10-18 15:53:39 +0400

    feat: Create LiveBox component for live preview

---
+++ src/features/RightPanel/LiveBox.tsx
@@ -0,0 +1,16 @@
+import { ViewTypes } from "../../types/ViewTypes";
+import ModalContent from "./ModalContent";
+
+const LiveBox = ({ activeView }: ViewTypes) => {
+  return (
+    <div className="border rounded-md border-gray-300 mt-5 p-4 bg-liveboxBg bg-cover bg-no-repeat">
+      <div className="bg-white inline-block px-4 py-2 rounded-md text-heading font-medium mb-7">
+        Live preview
+      </div>
+      <div className="flex justify-center">
+        <ModalContent activeView={activeView} />
+      </div>
+    </div>
+  );
+};
+export default LiveBox;
