3f404bc79297ead7c2e0fa9b1566580f8788d2bc
Sandro <vipsandro11@gmail.com>
2023-10-18 15:51:42 +0400

    feat: Update LeftPanel to accept an activeView prop

--- src/features/LeftPanel/LeftPanel.tsx
+++ src/features/LeftPanel/LeftPanel.tsx
@@ -4,12 +4,13 @@ import Clock from "./Clock";
 import Battery from "./Battery";
 import MessageWith from "./MessageWith";
 import MessageSenderSelector from "./MessageSenderSelector";
+import { ViewTypes } from "../../types/ViewTypes";
 
-const LeftPanel = () => {
+const LeftPanel = ({ activeView }: ViewTypes) => {
   return (
     <div className="border border-gray-300 p-4 flex-1 left-panel">
       <div className="flex gap-5 mb-5">
-        <Parts />
+        <Parts activeView={activeView} />
         <Layout />
       </div>
       <div className="flex gap-5 mb-5">
