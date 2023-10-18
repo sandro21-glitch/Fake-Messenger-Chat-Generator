a53b111bb0993d00c67d3b6ed6ec849172011e31
Sandro <vipsandro11@gmail.com>
2023-10-18 15:54:19 +0400

    feat: Update RightPanel component to accept and pass view-related props

--- src/features/RightPanel/RightPanel.tsx
+++ src/features/RightPanel/RightPanel.tsx
@@ -1,11 +1,14 @@
 import LiveBox from "./LiveBox";
 import ViewTabHeader from "./ViewTabHeader";
-
-const RightPanel = () => {
+import { ViewTypes } from "../../types/ViewTypes";
+interface View extends ViewTypes {
+  setActiveView: (view: string) => void;
+}
+const RightPanel = ({ activeView, setActiveView }: View) => {
   return (
     <div className="w-full">
-      <ViewTabHeader />
-      <LiveBox />
+      <ViewTabHeader activeView={activeView} setActiveView={setActiveView} />
+      <LiveBox activeView={activeView} />
     </div>
   );
 };
