95b9cdaf0bca2dbd37725a676f719ed87373df46
Sandro <vipsandro11@gmail.com>
2023-10-18 15:57:11 +0400

    feat: Create AddMessageBtn component for adding new messages

---
+++ src/ui/AddMessageBtn.tsx
@@ -0,0 +1,13 @@
+import { BiMessageSquareAdd } from "react-icons/bi";
+
+const AddMessageBtn = () => {
+  return (
+    <button className="bg-teal text-heading mb-2 text-white p-2 rounded-md hover:opacity-70 transition-opacity ease-in duration-100">
+      <div className="flex items-center gap-2">
+        Add Message
+        <BiMessageSquareAdd />
+      </div>
+    </button>
+  );
+};
+export default AddMessageBtn;
