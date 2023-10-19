77205a583eabaa99afb59b4cba829428b253f68d
Sandro <vipsandro11@gmail.com>
2023-10-19 17:43:07 +0400

    refactor: Dynamically render messages in ChatBody based on context state

--- src/features/RightPanel/modal content/ChatBody.tsx
+++ src/features/RightPanel/modal content/ChatBody.tsx
@@ -1,14 +1,22 @@
+import { useChatContext } from "../../../context/ChatContext";
 import MessageReceived from "./MessageReceived";
 import MessageSent from "./MessageSent";
 
 const ChatBody = () => {
+  const { state } = useChatContext();
+  const { allMessages } = state;
   return (
     <div className="bg-white px-4 py-2 min-h-[453px]">
       <div>
-        {/* received message */}
-        <MessageReceived />
-        {/* sent message */}
-        <MessageSent />
+        {allMessages.map((message, index) => {
+          if (message.from === "PersonOne") {
+            /* received message */
+            return <MessageReceived key={index} message={message.text} />;
+          } else {
+            /* sent message */
+            return <MessageSent key={index} message={message.text} index={index} />;
+          }
+        })}
       </div>
     </div>
   );
