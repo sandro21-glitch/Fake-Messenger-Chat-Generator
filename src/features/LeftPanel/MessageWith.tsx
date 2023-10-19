441c167e2dec22819835ea45be48d4313b5cbd77
Sandro <vipsandro11@gmail.com>
2023-10-19 17:41:16 +0400

    feat: Enhance MessageWith component for dynamic message recipient name

--- src/features/LeftPanel/MessageWith.tsx
+++ src/features/LeftPanel/MessageWith.tsx
@@ -1,6 +1,13 @@
+import { useState, useEffect } from "react";
 import TextHeading from "../../ui/TextHeading";
+import { useChatContext } from "../../context/ChatContext";
 
 const MessageWith = () => {
+  const { updateName } = useChatContext();
+  const [name, setName] = useState("Sandro Papidze");
+  useEffect(() => {
+    updateName(name);
+  }, [name]);
   return (
     <div className="w-full mb-5">
       <TextHeading text="Direct message with" />
@@ -9,7 +16,8 @@ const MessageWith = () => {
           type="text"
           name=""
           id=""
-          value={"Sandro Papidze"}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
           className="outline-none border w-full h-[30px] p-2 rounded-md text-labels"
         />
       </div>
