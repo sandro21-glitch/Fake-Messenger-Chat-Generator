0b3d8f2c9a9a864557217215f93c255d56797c51
Sandro <vipsandro11@gmail.com>
2023-10-19 17:40:39 +0400

    feat: Update Battery component to manage and display battery value dynamically

--- src/features/LeftPanel/Battery.tsx
+++ src/features/LeftPanel/Battery.tsx
@@ -1,15 +1,28 @@
+import { useEffect, useState } from "react";
 import TextHeading from "../../ui/TextHeading";
+import { useChatContext } from "../../context/ChatContext";
 
 const Battery = () => {
+  const { updateBatteryValue } = useChatContext();
+  const [batteryValue, setBatteryValue] = useState<number>(50);
+  const handleBtteryValue = (e: React.FormEvent<HTMLInputElement>) => {
+    const newValue = parseInt(e.currentTarget.value, 10);
+    if (!isNaN(newValue)) {
+      setBatteryValue(newValue);
+    }
+  };
+  useEffect(() => {
+    updateBatteryValue(batteryValue);
+  }, [batteryValue]);
   return (
     <div className="w-1/2">
       <TextHeading text="battery" />
       <div>
         <input
-          type="text"
-          placeholder="50"
+          type="number"
           className="border outline-none p-1 w-full rounded-md h-[30px] text-labels"
-          value={50}
+          value={batteryValue}
+          onChange={handleBtteryValue}
         />
       </div>
     </div>
