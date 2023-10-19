c723122ca47ee903dfdf9ddf82dd9c9e6c5ebcff
Sandro <vipsandro11@gmail.com>
2023-10-19 17:39:55 +0400

    refactor: Update exported actions and remove type assertion

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,2 +1,8 @@
-export const TOGGLE_CHECKBOX: string = "TOGGLE_CHECKBOX" as const;
-export const TOGGLE_LAYOUT: string = "TOGGLE_LAYOUT" as const;
+export const TOGGLE_CHECKBOX: string = "TOGGLE_CHECKBOX";
+export const TOGGLE_LAYOUT: string = "TOGGLE_LAYOUT";
+export const BATTERY_VALUE: string = "BATTERY_VALUE";
+export const UPDATE_CLOCK: string = "UPDATE_CLOCK";
+export const UPDATE_NAME: string = "UPDATE_NAME";
+export const PERSON_ONE_MESSAGE: string = "PERSON_ONE_MESSAGE";
+export const PERSON_TWO_MESSAGE: string = "PERSON_TWO_MESSAGE";
+export const UPDATE_IMAGE: string = "UPDATE_IMAGE";
