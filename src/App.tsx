d74724292c09a30c1462c4f1d56ef086cd671a0f
Sandro <vipsandro11@gmail.com>
2023-10-19 17:38:59 +0400

    feat: Add useEffect to clear "image" key in local storage on app load

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./ui/Navbar";
 import Home from "./pages/Home";
 
 function App() {
+  // clear the "image" key in local storage when the app loads
+  useEffect(() => {
+    localStorage.setItem("image", "/profile.png");
+  }, []);
   return (
     <BrowserRouter>
       <Navbar />
