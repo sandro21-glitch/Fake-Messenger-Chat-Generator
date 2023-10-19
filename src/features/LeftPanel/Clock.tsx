33ebbe328fef50bd12737829e712f666cf8f62c7
Sandro <vipsandro11@gmail.com>
2023-10-19 17:40:56 +0400

    feat: Enhance Clock component for dynamic clock value

--- src/features/LeftPanel/Clock.tsx
+++ src/features/LeftPanel/Clock.tsx
@@ -1,6 +1,12 @@
+import { useState, useEffect } from "react";
 import TextHeading from "../../ui/TextHeading";
-
+import { useChatContext } from "../../context/ChatContext";
 const Clock = () => {
+  const { updateClockValue } = useChatContext();
+  const [clock, setClock] = useState("10:04 am");
+  useEffect(() => {
+    updateClockValue(clock);
+  }, [clock]);
   return (
     <div className="w-1/2">
       <TextHeading text="clock" />
@@ -9,7 +15,8 @@ const Clock = () => {
           type="text"
           placeholder="10:04 am"
           className="border outline-none p-1 w-full rounded-md h-[30px] text-labels"
-          value={"10:04 am"}
+          value={clock}
+          onChange={(e) => setClock(e.target.value)}
         />
       </div>
     </div>
