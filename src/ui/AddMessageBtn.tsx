16ecdce735a4df0c029f7647a9a25fcae6c90c93
Sandro <vipsandro11@gmail.com>
2023-10-19 17:46:51 +0400

    refactor: Added type="submit" attribute to button

--- src/ui/AddMessageBtn.tsx
+++ src/ui/AddMessageBtn.tsx
@@ -2,7 +2,10 @@ import { BiMessageSquareAdd } from "react-icons/bi";
 
 const AddMessageBtn = () => {
   return (
-    <button className="bg-teal text-heading mb-2 text-white p-2 rounded-md hover:opacity-70 transition-opacity ease-in duration-100">
+    <button
+      type="submit"
+      className="bg-teal text-heading mb-2 text-white p-2 rounded-md hover:opacity-70 transition-opacity ease-in duration-100"
+    >
       <div className="flex items-center gap-2">
         Add Message
         <BiMessageSquareAdd />
