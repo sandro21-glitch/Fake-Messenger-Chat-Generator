5226c51482c6ebf9f63d954206a178c93d245f45
Sandro <vipsandro11@gmail.com>
2023-10-19 17:44:12 +0400

    feat: Enhance MessageSent component with dynamic message content and fade-in animation

--- src/features/RightPanel/modal content/MessageSent.tsx
+++ src/features/RightPanel/modal content/MessageSent.tsx
@@ -1,18 +1,21 @@
 import messageSent from "/message-sent.png";
-
-const MessageSent = () => {
+type MessageType = {
+  message: string;
+  index: number;
+};
+const MessageSent = ({ message, index }: MessageType) => {
   return (
-    <div className="flex justify-end w-full relative py-2">
+    <div className="flex justify-end w-full relative pt-[.75rem]">
       <div
         className="text-labels bg-fbBlue text-white border border-fbLightBlue
-     rounded-[20px] py-[6px] px-[10px] w-fit max-w-[240px] min-h-[30px] break-words"
+     rounded-[20px] py-[6px] px-[10px] w-fit max-w-[240px] min-h-[30px] break-words fade-in-text "
       >
-        კარგად შენ?
+        {message}
       </div>
-      <div className="absolute right-[-9px] bottom-[-1px]">
+      <div className="absolute right-[-9px] bottom-[-8px]">
         <img
           src={messageSent}
-          alt=""
+          alt="message"
           className="w-[11px] h-[11px] opacity-[.4]"
         />
       </div>
