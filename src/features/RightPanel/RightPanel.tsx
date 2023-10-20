95b38c99af1fdd8168652e023f7ad5614d9049bb
Sandro <vipsandro11@gmail.com>
2023-10-20 15:31:07 +0400

    feat: Add DownloadBtn component to RightPanel

--- src/features/RightPanel/RightPanel.tsx
+++ src/features/RightPanel/RightPanel.tsx
@@ -1,6 +1,7 @@
 import LiveBox from "./LiveBox";
 import ViewTabHeader from "./ViewTabHeader";
 import { ViewTypes } from "../../types/ViewTypes";
+import DownloadBtn from "../../ui/DownloadBtn";
 interface View extends ViewTypes {
   setActiveView: (view: string) => void;
 }
@@ -9,6 +10,7 @@ const RightPanel = ({ activeView, setActiveView }: View) => {
     <div className="w-full">
       <ViewTabHeader activeView={activeView} setActiveView={setActiveView} />
       <LiveBox activeView={activeView} />
+      <DownloadBtn />
     </div>
   );
 };
