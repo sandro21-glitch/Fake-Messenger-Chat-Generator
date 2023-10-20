00ca6a47e7dbc9a040f4f0c4d06bd0a1af5a9428
Sandro <vipsandro11@gmail.com>
2023-10-20 15:28:12 +0400

    feat: Enhance MessageSent component to display message status icons

--- src/features/RightPanel/modal content/MessageSent.tsx
+++ src/features/RightPanel/modal content/MessageSent.tsx
@@ -1,24 +1,53 @@
+import { useEffect, useState } from "react";
+import { useChatContext } from "../../../context/ChatContext";
 import messageSent from "/message-sent.png";
+import messageDeliverd from "/message-deliverd.png";
 type MessageType = {
   message: string;
   index: number;
 };
+
 const MessageSent = ({ message, index }: MessageType) => {
+  const { state } = useChatContext();
+  const { allMessages, status, img } = state;
+
+  const [lastMessageIndex, setLastMessageIndex] = useState(-1);
+
+  useEffect(() => {
+    for (let i = allMessages.length - 1; i >= 0; i--) {
+      if (allMessages[i].from === "PersonTwo") {
+        setLastMessageIndex(i);
+        break;
+      }
+    }
+  }, [allMessages]);
   return (
     <div className="flex justify-end w-full relative pt-[.75rem]">
       <div
         className="text-labels bg-fbBlue text-white border border-fbLightBlue
      rounded-[20px] py-[6px] px-[10px] w-fit max-w-[240px] min-h-[30px] break-words fade-in-text "
       >
-        {message}
+        <span>{message}</span>
       </div>
-      <div className="absolute right-[-9px] bottom-[-8px]">
-        <img
-          src={messageSent}
-          alt="message"
-          className="w-[11px] h-[11px] opacity-[.4]"
-        />
-      </div>
+      {index === lastMessageIndex ? (
+        <div className="absolute right-[-9px] bottom-[-8px] fade-in-text rounded-full overflow-hidden">
+          <img
+            src={
+              status.sent
+                ? messageSent
+                : status.delivered
+                ? messageDeliverd
+                : status.seen
+                ? img
+                : ""
+            }
+            alt="message"
+            className={`w-[11px] h-[11px] ${
+              status.seen === true ? "opacity-100" : "opacity-[.6]"
+            } `}
+          />
+        </div>
+      ) : null}
     </div>
   );
 };
