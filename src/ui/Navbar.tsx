55d02985f23937efd8b59c8ea3d9d83469aae503
Sandro <vipsandro11@gmail.com>
2023-10-20 15:24:56 +0400

    refactor: Update Navbar component to include Facebook Messenger icon

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -1,14 +1,19 @@
 import logo from "/logo/logo-no-background.png";
+import { FaFacebookMessenger } from "react-icons/fa";
 const Navbar = () => {
   return (
     <header className="bg-white shadow-lg">
       <nav className="flex-between section-center p-4">
-        <img src={logo} alt="logo" className="w-full max-w-[3.5rem] cursor-pointer" />
-        <ul className="flex-center gap-5">
-          <li className="hover:text-teal cursor-pointer transition-colors ease-in duration-150">Mail</li>
-          <li className="hover:text-teal cursor-pointer transition-colors ease-in duration-150">IMEI</li>
-          <li className="hover:text-teal cursor-pointer transition-colors ease-in duration-150">Personal Detail </li>
-          <li className="hover:text-teal cursor-pointer transition-colors ease-in duration-150">Messenger</li>
+        <img
+          src={logo}
+          alt="logo"
+          className="w-full max-w-[3.5rem] cursor-pointer"
+        />
+        <ul>
+          <li className="hover:text-teal cursor-pointer transition-colors ease-in duration-150 flex items-center gap-1">
+            Messenger
+            <FaFacebookMessenger />
+          </li>
         </ul>
       </nav>
     </header>
