adcf4ccd28be87671e8f570e7612cfa090df41e1
Sandro <vipsandro11@gmail.com>
2023-10-20 15:30:45 +0400

    feat: Add default messages to ChatBody when allMessages is empty

--- src/features/RightPanel/modal content/ChatBody.tsx
+++ src/features/RightPanel/modal content/ChatBody.tsx
@@ -1,23 +1,32 @@
 import { useChatContext } from "../../../context/ChatContext";
 import MessageReceived from "./MessageReceived";
 import MessageSent from "./MessageSent";
-
 const ChatBody = () => {
   const { state } = useChatContext();
   const { allMessages } = state;
+  if (allMessages.length === 0) {
+    return (
+      <div className="bg-white px-4 py-2 min-h-[453px]">
+        {/* Render your default message when allMessages is empty */}
+        <MessageReceived key={0} message="Hi👋" />
+        <MessageSent key={1} message={"Hi"} index={1} />
+      </div>
+    );
+  }
+
   return (
     <div className="bg-white px-4 py-2 min-h-[453px]">
-      <div>
-        {allMessages.map((message, index) => {
-          if (message.from === "PersonOne") {
-            /* received message */
-            return <MessageReceived key={index} message={message.text} />;
-          } else {
-            /* sent message */
-            return <MessageSent key={index} message={message.text} index={index} />;
-          }
-        })}
-      </div>
+      {allMessages.map((message, index) => {
+        if (message.from === "PersonOne") {
+          /* received message */
+          return <MessageReceived key={index} message={message.text} />;
+        } else {
+          /* sent message */
+          return (
+            <MessageSent key={index} message={message.text} index={index} />
+          );
+        }
+      })}
     </div>
   );
 };
