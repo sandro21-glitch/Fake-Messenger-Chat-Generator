70e679734702331093ea21a8821366268cdfdf2c
Sandro <vipsandro11@gmail.com>
2023-10-20 15:32:39 +0400

    refactor: Reorganize message status section in PersonTwo component

--- src/features/LeftPanel/change person/PersonTwo.tsx
+++ src/features/LeftPanel/change person/PersonTwo.tsx
@@ -1,5 +1,6 @@
 import TextHeading from "../../../ui/TextHeading";
 import PersonTwoForm from "./PersonTwoForm";
+import PersonTwoStatus from "./PersonTwoStatus";
 
 const PersonTwo = () => {
   return (
@@ -8,31 +9,7 @@ const PersonTwo = () => {
       {/* message status */}
       <div className="">
         <TextHeading text="Message Status :" />
-        <div className="space-y-1 my-2">
-          <div className="flex items-center">
-            <input type="radio" name="sent" id="sent" className="mr-1" />
-            <label htmlFor="sent" className="text-labels">
-              Sent
-            </label>
-          </div>
-          <div className="flex items-center">
-            <input
-              type="radio"
-              name="delivered"
-              id="delivered"
-              className="mr-1"
-            />
-            <label htmlFor="delivered" className="text-labels">
-              Delivered
-            </label>
-          </div>
-          <div className="flex items-center">
-            <input type="radio" name="seen" id="seen" className="mr-1" />
-            <label htmlFor="seen" className="text-labels">
-              Seen
-            </label>
-          </div>
-        </div>
+        <PersonTwoStatus />
       </div>
     </div>
   );
